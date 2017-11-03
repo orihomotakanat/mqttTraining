431bb92b4c0fb816622c13ef1117a1d2c238a35d
Tomohiro TANAKA <tanaka1201t@gmail.com>
2017-11-03 15:21:16 +0900

    Add function of processing commands

--- drone_python/drone.py
+++ drone_python/drone.py
@@ -93,7 +93,81 @@ class DroneCommandProcessor:
         client.subscribe(DroneCommandProcessor.commands_topic, qos=2)
         client.publish(topic=DroneCommandProcessor.commands_topic, payload="{} is listening to messages".format(DroneCommandProcessor.active_instance.name)
 
-    def onMessage(client, userdata, msg)
+    @staticmethod
+    def onMessage(client, userdata, msg):
+        payloadString = msg.payload.decode('utf-8')
+        if msg.topic == DroneCommandProcessor.commands_topic:
+            print("I've received the following msg: {0}".format(payloadString))
+        try:
+            msgDictionary = json.load(payloadString)
+            if COMMAND_KEY in msgDictionary:
+                command = msgDictionary[COMMAND_KEY]
+                drone = DroneCommandProcessor.active_instance.drone
+                is_command_processed = False
+                if command == CMD_TAKE_OFF:
+                    drone.take_off()
+                    is_command_processed = True
+                elif command == CMD_LAND:
+                    drone.land()
+                    is_command_processed = True
+                elif command == CMD_LAND_IN_SAFE_PLACE:
+                    drone.land_in_safe_place()
+                    is_command_processed = True
+                elif command == CMD_MOVE_UP:elif command == CMD_LAND:
+                    drone.land()
+                    is_command_processed = True
+                elif command == CMD_LAND_IN_SAFE_PLACE:
+                    drone.land_in_safe_place()
+                    is_command_processed = True
+                elif command == CMD_MOVE_UP:
+                    drone.move_up()
+                    is_command_processed = True
+                elif command == CMD_MOVE_DOWN:
+                    drone.move_down()
+                    is_command_processed = True
+                elif command == CMD_MOVE_FORWARD:
+                    drone.move_forward()
+                    is_command_processed = True
+                elif command == CMD_MOVE_BACK:
+                    drone.move_back()
+                    is_command_processed = True
+                elif command == CMD_MOVE_LEFT:
+                    drone.move_left()
+                    is_command_processed = True
+                elif command == CMD_MOVE_RIGHT:
+                    drone.move_right()
+                    is_command_processed = True
+                elif command == CMD_ROTATE_RIGHT:
+                    degrees = msgDictionary[KEY_DEGREES]
+                    drone.rotate_right(degrees)
+                    is_command_processed = True
+                elif command == CMD_ROTATE_LEFT:
+                    degrees = msgDictionary[KEY_DEGREES]
+                    drone.rotate_left(degrees)
+                    is_command_processed = True
+                elif command == CMD_SET_MAX_ALTITUDE:
+                    feet = msgDictionary[KEY_FEET]
+                    drone.set_max_altitude(feet)
+                    is_command_processed = True
+                elif command == CMD_SET_MIN_ALTITUDE:
+                    feet = msgDictionary[KEY_FEET]
+                    drone.set_min_altitude(feet)
+                    is_command_processed = True
+                if is_command_processed:
+                    DroneCommandProcessor.active_instance.publishResponseMessage(
+                            msgDictionary)
+                else:
+                    print("The message includes an unknown command.")
+        except ValueError: # Msg is not a dictionary or no json object could be decoded
+            print("No including a valid command")
+
+    def publishResponseMessage(self, message):
+        responseMessage = json.dumps({
+            SUCCESSFULLY_PROCESSED_COMMAND_KEY: message[COMMAND_KEY]})
+        result = self.clien.publish(
+            topic = self.__class__.processed_commands_topic,
+            payload = responseMessage)
+        return result
 
     # Debug
     def onLog(client, userdata, level, buf):
