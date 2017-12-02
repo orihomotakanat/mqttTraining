ffff30c6b1a36a1ac7a35aa131968aa11f9f2fe9
Tomohiro TANAKA <tanaka1201t@gmail.com>
2017-12-02 17:30:07 +0900

    Debug all function

--- drone_python/iotBoardClient.py
+++ drone_python/iotBoardClient.py
@@ -22,34 +22,34 @@ processed_commands_topic = "processedcommands/{}".format(drone_name)
 class LoopControl:
     is_last_command_processed = False
 
-    def onConnect(client, userdata, rc):
-        print("Connect result: {}".format(mqtt.connack_string(rc)))
-        client.subscribe(processed_commands_topic)
+def onConnect(client, userdata, rc):
+    print("Connect result: {}".format(mqtt.connack_string(rc)))
+    client.subscribe(processed_commands_topic)
 
-    def onMessage(client,userdata,msg):
-        if msg.topic == processed_commands_topic:
-            payloadString = msg.payload.decode('utf-8')
-            print(payloadString)
+def onMessage(client,userdata,msg):
+    if msg.topic == processed_commands_topic:
+        payloadString = msg.payload.decode('utf-8')
+        print(payloadString)
 
-            if payloadString.count(CMD_LAND_IN_SAFE_PLACE) > 0:
-                LoopControl.is_last_command_processed = True
+    if payloadString.count(CMD_LAND_IN_SAFE_PLACE) > 0:
+        LoopControl.is_last_command_processed = True
 
-    def onSubscribe(client, userdata, mid, granted_qos):
-        print("Subscribed with QoS: {}, Mid: {}".format(granted_qos[0], mid))
+def onSubscribe(client, userdata, mid, granted_qos):
+    print("Subscribed with QoS: {}, Mid: {}".format(granted_qos[0], mid))
 
-    def publishCommand(client, command_name, key="", value=""):
-        if key:
-            command_meesage = json.dumps({
-                COMMAND_KEY: command_name, key: value})
-        else:
-            command_meesage = json.dumps({
-                COMMAND_KEY: command_name})
-        result = client.publish(topic = commands_topic, payload = command_meesage, qos = 2)
-        return result
+def publishCommand(client, command_name, key="", value=""):
+    if key:
+        command_meesage = json.dumps({
+            COMMAND_KEY: command_name, key: value})
+    else:
+        command_meesage = json.dumps({
+            COMMAND_KEY: command_name})
+    result = client.publish(topic = commands_topic, payload = command_meesage, qos = 2)
+    return result
 
-    # Debug
-    def onLog(client, userdata, level, buf):
-        print("Log: {}".format(buf))
+# Debug
+def onLog(client, userdata, level, buf):
+    print("Log: {}".format(buf))
 
 if __name__ == "__main__":
     client = mqtt.Client(protocol=mqtt.MQTTv311)
