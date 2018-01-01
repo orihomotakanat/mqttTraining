5fa99c8322ec4e95d289e9889077ad90c18f08e3
Tomohiro TANAKA <tanaka1201t@gmail.com>
2018-01-02 00:09:40 +0900

    Add instruction

---
+++ sensor_java/classes/sensors/README.md
@@ -0,0 +1,28 @@
+# Sensor directory
+Components
+```
+sensors
+    ├── README.md
+    ├── pom.xml
+    └── src
+        └── main
+            ├── SecurityHelper.java
+            └── java
+```
+
+## `SecurityHelper.java`
+### `SecurityHelper` class
+This class provides many static methods which makes it easy to establish a TLS secured connection with MQTT server. The public static `createSocketFactory` method receives the file name for:
+* Certificate authority certificate
+* Client certiicate
+* Client key
+
+The method loads all these files, generate the appropriate instances from them, and return an instance of `java.net.ssl.SSLSocketFactory`. In detail, this class provides the following private static method:
+* `getFactoryInstance`
+* `createX509CertificateFromFile`
+* `createPrivateKeyFromPemFile`
+* `createKeyManagerFactory`
+* `creeateTrustManagerFactory`
+
+
+And, provides public static `createSocketFactory` method.
--- sensor_java/classes/sensors/src/main/SecurityHelper.java
+++ sensor_java/classes/sensors/src/main/SecurityHelper.java
@@ -35,10 +35,12 @@ import org.bouncycastle.util.io.pem.PemReader;
 public class SecurityHelper {
     private final static String TLS_VERSION = "TLSv1.2";
 
+    //converting pub/priv keys of the RSA alogorithm
     private static KeyFactory getKeyFactoryInstance() throws NoSuchAlgorithmException {
         return KeyFactory.getInstance("RSA");
     }
 
+    //Receiving cert files and Calling other methos
     private static X509Certificate create509CertificateFromFile (final String certificateFileName) throws IOException, CertificateException {
         // Loads an X509 cert from the specified certificate file name
         final File file = new java.io.File(certificateFileName);
@@ -55,7 +57,46 @@ public class SecurityHelper {
         return certificate;
     }
 
+    //Receiving key file name in the PEM format and Generating an instance
     private static PrivateKey createPrivateKeyFromPemFile(final String keyFileName) throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
         // Loads a privte-key from the specified key file name
+        final PemReader pemReader = new PemReader(new FileReader(keyFileName));
+        final PemObject pemObject = pemReader.readPemObject();
+        final byte[] pemContent = pemObject.getContent();
+        pemReader.close();
+        final PKCS8EncodedKeySpec encodedKeySpec = new PKCS8EncodedKeySpec(pemContent);
+        final KeyFactory keyFactory = getKeyFactoryInstance();
+        final PrivateKey privateKey = keyFactory.generatePrivate(encodedKeySpec);
+        return privateKey;
+
     }
+
+    //...
+    private static KeyManagerFactory createKeyManagerFactory (
+            final String clientCertificateFileName,
+            final String clientKeyFileNeme,
+            final String clientKeyPassword) throws InvalidKeySpecException, NoSuchAlgorithmException, KeyStoreException, IOException, CertificateException, UnrecoverableKeyException {
+        // Add contents
+    }
+
+    //..
+    private static TrustManagerFactory createTrustManagerFactory (final String caCertificateFileName)
+        throws CertificateExeption, NouchAlgorithmException, IOException, KeyStoreException {
+        // Add contents
+    }
+
+    //...
+    public static SSLSocketFactory createSocketFactory (
+            final String caCertificateFileName,
+            final String clientCertificateFileName,
+            final String clientKeyFileName) throws Exception {
+        final String clientKeyPassword = "";
+        try {
+
+        } // try end
+        catch (Exception e) {
+
+        } // catch end
+    }
+    
 } //public class SecurityHelper end
